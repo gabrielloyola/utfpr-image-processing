628160cfc6426e48def4a8b4e48d61810f7b2e76
Gabriel Loyola <gabrielfrml@gmail.com>
2022-08-18 04:35:11 -0300

    implements "binariza"

--- projeto_1/main.py
+++ projeto_1/main.py
@@ -17,13 +17,13 @@ INPUT_IMAGE =  'arroz.bmp'
 # TODO: ajuste estes parâmetros!
 NEGATIVO = False
 THRESHOLD = 0.4
-ALTURA_MIN = 1
-LARGURA_MIN = 1
-N_PIXELS_MIN = 1
+ALTURA_MIN = 15
+LARGURA_MIN = 15
+N_PIXELS_MIN = 500
 
 #===============================================================================
 
-def binariza (img, threshold):
+def binariza(img, threshold):
     ''' Binarização simples por limiarização.
 
 Parâmetros: img: imagem de entrada. Se tiver mais que 1 canal, binariza cada
@@ -32,13 +32,11 @@ Parâmetros: img: imagem de entrada. Se tiver mais que 1 canal, binariza cada
             
 Valor de retorno: versão binarizada da img_in.'''
 
-    # TODO: escreva o código desta função.
-    # Dica/desafio: usando a função np.where, dá para fazer a binarização muito
-    # rapidamente, e com apenas uma linha de código!
+    return np.where(img < threshold, 0, 1)
 
 #-------------------------------------------------------------------------------
 
-def rotula (img, largura_min, altura_min, n_pixels_min):
+def rotula(img, largura_min, altura_min, n_pixels_min):
     '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
 [0.1,0.2,etc].
 
@@ -47,7 +45,7 @@ Parâmetros: img: imagem de entrada E saída.
             altura_min: descarta componentes com altura menor que esta.
             n_pixels_min: descarta componentes com menos pixels que isso.
 
-Valor de retorno: uma lista, onde cada item é um vetor associativo (dictionary)
+Valor de retorno: uma lista, onde cada item é um vetor associativo(dictionary)
 com os seguintes campos:
 
 'label': rótulo do componente.
@@ -60,46 +58,46 @@ respectivamente: topo, esquerda, baixo e direita.'''
 
 #===============================================================================
 
-def main ():
+def main():
 
     # Abre a imagem em escala de cinza.
-    img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
     if img is None:
-        print ('Erro abrindo a imagem.\n')
-        sys.exit ()
+        print('Erro abrindo a imagem.\n')
+        sys.exit()
 
     # É uma boa prática manter o shape com 3 valores, independente da imagem ser
     # colorida ou não. Também já convertemos para float32.
-    img = img.reshape ((img.shape [0], img.shape [1], 1))
-    img = img.astype (np.float32) / 255
+    img = img.reshape((img.shape[0], img.shape[1], 1))
+    img = img.astype(np.float32) / 255
 
     # Mantém uma cópia colorida para desenhar a saída.
-    img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
+    img_out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
     # Segmenta a imagem.
     if NEGATIVO:
         img = 1 - img
-    img = binariza (img, THRESHOLD)
-    cv2.imshow ('01 - binarizada', img)
-    cv2.imwrite ('01 - binarizada.png', img*255)
+    img = binariza(img, THRESHOLD)
+    cv2.imshow('01 - binarizada', img)
+    cv2.imwrite('01 - binarizada.png', img*255)
 
-    start_time = timeit.default_timer ()
-    componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
-    n_componentes = len (componentes)
-    print ('Tempo: %f' % (timeit.default_timer () - start_time))
-    print ('%d componentes detectados.' % n_componentes)
+    start_time = timeit.default_timer()
+    componentes = rotula(img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
+    n_componentes = len(componentes)
+    print('Tempo: %f' %(timeit.default_timer() - start_time))
+    print('%d componentes detectados.' % n_componentes)
 
     # Mostra os objetos encontrados.
     for c in componentes:
-        cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+        cv2.rectangle(img_out,(c['L'], c['T']),(c['R'], c['B']),(0,0,1))
 
-    cv2.imshow ('02 - out', img_out)
-    cv2.imwrite ('02 - out.png', img_out*255)
-    cv2.waitKey ()
-    cv2.destroyAllWindows ()
+    cv2.imshow('02 - out', img_out)
+    cv2.imwrite('02 - out.png', img_out*255)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    main ()
+    main()
 
 #===============================================================================
