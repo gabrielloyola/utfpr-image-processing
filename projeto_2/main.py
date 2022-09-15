96523ac30bc2c3b52af003950723f464ab81e45f
Gabriel Loyola <gabrielfrml@gmail.com>
2022-09-15 15:46:13 -0300

    wip

--- projeto_2/main.py
+++ projeto_2/main.py
@@ -11,74 +11,69 @@ import cv2
 
 #===============================================================================
 
-INPUT_IMAGE =  'Exemplos/b - Original.bmp'
-LARGURA_JANELA = 11
-ALTURA_JANELA = 15
+INPUT_IMAGE = 'a'
+LARGURA_JANELA = 3
+ALTURA_JANELA = 3
 
 def blur_basico(img, altura_janela, largura_janela):
-    largura = len(img[0])
-    altura = len(img)
-
+    altura, largura, n_canais = img.shape
     img_out = np.zeros(img.shape)
 
-    for c in range(0, 3):
-        for y in range(int(altura_janela / 2), int(altura - altura_janela / 2)):
-            start_y = int(y - altura_janela / 2)
-            end_y = max(int(y + altura_janela / 2), 1)
+    for canal in range(0, n_canais):
+        for y in range(int(altura_janela / 2), altura - int(altura_janela / 2)):
+            start_y = y - int(altura_janela / 2)
+            end_y = max(y + int(altura_janela / 2), 1) + 1
             for x in range(int(largura_janela / 2), int(largura - largura_janela / 2)):
-                start_x = int(x - largura_janela / 2)
-                end_x = max(int(x + largura_janela / 2), 1)
+                start_x = x - int(largura_janela / 2)
+                end_x = max(x + int(largura_janela / 2), 1) + 1
 
-                img_out[y][x][c] = calcula_media(img[start_y:end_y, start_x:end_x], c)
+                img_out[y][x][canal] = np.average(img[start_y:end_y, start_x:end_x][canal])
 
     return img_out
 
 def blur_separavel(img, altura_janela, largura_janela):
-    largura = len(img[0])
-    altura = len(img)
+    altura, largura, n_canais = img.shape
 
     img_out = np.zeros(img.shape)
 
-    for c in range(0, 3):
-        for y in range(int(altura_janela / 2), int(altura - altura_janela / 2)):
-            start_y = int(y - altura_janela / 2)
-            end_y = max(int(y + altura_janela / 2), 1)
+    for canal in range(0, n_canais):
+        for y in range(0, altura):
+            soma = img[y][0:largura_janela][canal].sum()
             for x in range(int(largura_janela / 2), int(largura - largura_janela / 2)):
-                start_x = int(x - largura_janela / 2)
-                end_x = max(int(x + largura_janela / 2), 1)
+                if y < 0:
+                    soma = soma - img[y-1][x][canal] + img[y + altura_janela][x][canal]
 
-                img_out[y][x][c] = calcula_media(img[start_y:end_y, start_x:end_x], c)
+                img_out[y][x][canal] = soma / (altura_janela * largura_janela)
+        
+        for x in range(0, largura):
+            soma = img_out[0:altura_janela][x][canal].sum()
+            for y in range(int(altura_janela / 2), int(altura - altura_janela / 2)):
+                if x < 0:
+                    soma = soma - img_out[y-1][x][canal] + img_out[y + largura_janela][x][canal]
+
+                img_out[y][x][canal] = soma / (altura_janela * largura_janela)
 
     return img_out
 
-def calcula_media(janela, channel):
-    largura = len(janela[0])
-    altura = len(janela)
-
-    soma = 0
-    for y in range(0, altura):
-        for x in range(0, largura):
-            soma += janela[y][x][channel]
-
-    return soma / (altura * largura)
 def imagem_integral(img):
-    largura = len(janela[0])
-    altura = len(janela)
+    altura, largura, n_canais = img.shape
     
     img_out = np.zeros(img.shape)
-    for c in range(0,3):
-        for y in range(0,altura):
-            img_out[y][0][c] =img[y][0][c]
-            for x in range(1,largura):
-                img_out[y][x][c] =img[y][x]+img_out[y][x-1][c]
-        for y in range(1,altura):
-            for x in range(0,largura):
+
+    for c in range(0, n_canais):
+        for y in range(0, altura):
+            img_out[y][0][c] = img[y][0][c]
+            for x in range(1, largura):
+                img_out[y][x][c] = img[y][x][c] + img_out[y][x-1][c]
+
+        for y in range(1, altura):
+            for x in range(0, largura):
                 img_out[y][x][c] = img_out[y][x][c] + img_out[y-1][x][c]
 
-    return img_out                
+    return img_out
 
 def main():
-    img = cv2.imread(INPUT_IMAGE)
+    img = cv2.imread("Exemplos/{} - Original.bmp".format(INPUT_IMAGE))
     if img is None:
         print('Erro ao abrir a imagem.\n')
         sys.exit()
@@ -90,15 +85,17 @@ def main():
     cv2.imwrite('01 - Original.bmp', img * 255)
 
     start_time = timeit.default_timer()
-    img = blur_basico(img, ALTURA_JANELA, LARGURA_JANELA)
+    img = blur_separavel(img, ALTURA_JANELA, LARGURA_JANELA)
     print('Tempo: %f' % (timeit.default_timer() - start_time))
 
     cv2.imshow('02 - out', img)
     cv2.imwrite('02 - out.png', img * 255)
 
-    img_base = cv2.imread("Exemplos/b - Borrada {}x{}.bmp".format(LARGURA_JANELA, ALTURA_JANELA))
+    img_base = cv2.imread("Exemplos/{} - Borrada {}x{}.bmp".format(INPUT_IMAGE, LARGURA_JANELA, ALTURA_JANELA))
 
-    cv2.imshow("Exemplos/b - Borrada {}x{}.bmp".format(LARGURA_JANELA, ALTURA_JANELA), img_base)
+    img_base = img_base.astype(np.float32) / 255
+
+    cv2.imshow("Exemplos/{} - Borrada {}x{}.bmp".format(INPUT_IMAGE, LARGURA_JANELA, ALTURA_JANELA), img_base)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
