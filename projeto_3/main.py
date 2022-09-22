58e483b804c4dd3329274f90841db5819e2fd937
Gabriel Loyola <gabrielfrml@gmail.com>
2022-09-21 22:50:02 -0300

    borra varias vezes para bloom media

--- projeto_3/main.py
+++ projeto_3/main.py
@@ -11,46 +11,79 @@ import cv2
 
 #===============================================================================
 
-THRESHOLD = .482
-SIGMA = 2
-INPUT_IMG = 'GT2.bmp'
-#INPUT_IMG = 'Wind Waker GC.bmp'
+THRESHOLD = .55
+INITIAL_SIGMA = 10
+KERNEL_SIZE = 15
+
+GAUSSIAN_ITERATIONS = 3
+MEAN_ITERATIONS = 5
+
+# INPUT_IMG = 'GT2.bmp'
+INPUT_IMG = 'Wind Waker GC.bmp'
+
 IMG_MULT = 0.9
-BLOOM_MULT = .05
+BLOOM_MULT = .15
+
+def gaussian_bloom(img):
+    bloom = np.zeros(img.shape)
+
+    sigma = INITIAL_SIGMA
+
+    for _i in range(0, GAUSSIAN_ITERATIONS):
+        bloom += cv2.GaussianBlur(img, (KERNEL_SIZE, KERNEL_SIZE), sigma)
+        sigma *= 2
+
+    return bloom
+
+def mean_bloom(img):
+    bloom = np.zeros(img.shape)
+
+    for _i in range(0, GAUSSIAN_ITERATIONS):
+        mean_bloom = cv2.blur(img, (KERNEL_SIZE, KERNEL_SIZE))
+        for _j in range(0, MEAN_ITERATIONS - 1):
+            mean_bloom = cv2.blur(mean_bloom, (KERNEL_SIZE, KERNEL_SIZE))
+
+        bloom += mean_bloom
+
+    return bloom
 
 def main():
     img = cv2.imread(INPUT_IMG)
     if img is None:
         print('Erro ao abrir a imagem.\n')
         sys.exit()
-    mask = cv2.imread(INPUT_IMG,cv2.IMREAD_GRAYSCALE)
-    mask = mask.reshape((mask.shape[0], mask.shape[1], 1))
+
+    mask = cv2.imread(INPUT_IMG, cv2.IMREAD_GRAYSCALE)
+    mask = mask.reshape(mask.shape[0], mask.shape[1], 1)
+
     # Convertendo para float32.
     img = img.astype(np.float32) / 255
     mask = mask.astype(np.float32) / 255
-    
+
     cv2.imshow('01 - Original', img)
-    cv2.imwrite('01 - Original.bmp', img * 255)
-    
+
     img_limiar = np.where(mask > THRESHOLD, img, 0)
 
     cv2.imshow('02 - limiar', img_limiar)
-    bloom_media = np.zeros(img.shape)
-    for i in range(0,4):
-      bloom_media += cv2.blur(img_limiar,(19,19))
-    cv2.imshow('BLOOM FILTRO DA MEDIA', bloom_media)  
 
-    k = 33
-    bloom = np.zeros(img.shape)
-    for sigma_mult in [1, 2, 4, 8, 16, 32, 64, 128]:
-        sg = k * sigma_mult
-        bloom += cv2.GaussianBlur(img_limiar, (k, k), sg)
+    bloom = gaussian_bloom(img_limiar)
 
-    cv2.imshow('BLOOOOM', bloom)
+    cv2.imshow('03 - Bloom', bloom)
+    cv2.imwrite('03 - Bloom.bmp', bloom * 255)
 
-    img = img * IMG_MULT + bloom_media * BLOOM_MULT
+    bloom_media = mean_bloom(img_limiar)
 
-    cv2.imshow('OUT', img)
+    cv2.imshow('04 - Bloom Media', bloom_media)
+    cv2.imwrite('04 - Bloom Media.bmp', bloom_media * 255)
+
+    img_out = img * IMG_MULT + bloom * BLOOM_MULT
+    img_out_mean = img * IMG_MULT + bloom_media * BLOOM_MULT
+
+    cv2.imshow('05 - OUT Gaussiano', img_out)
+    cv2.imwrite('05 - OUT Gaussiano.bmp', img_out * 255)
+
+    cv2.imshow('06 - OUT Media', img_out_mean)
+    cv2.imwrite('06 - OUT Media.bmp', img_out_mean * 255)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
